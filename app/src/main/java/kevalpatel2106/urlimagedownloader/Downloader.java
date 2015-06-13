fada87ffadde17f9f3f996b59f74e3281f1ac3be
Keval Patel <kevalpatel2106@gmail.com>
2015-06-12 21:37:52 -0700

    1. Handling imageView display

--- app/src/main/java/kevalpatel2106/urlimagedownloader/Downloader.java
+++ app/src/main/java/kevalpatel2106/urlimagedownloader/Downloader.java
@@ -1,6 +1,8 @@
 package kevalpatel2106.urlimagedownloader;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ImageView;
@@ -18,88 +20,120 @@ import java.util.ArrayList;
 
 
 /**
- * Created by KP on 14-Apr-15.
+ * Created by KP on 12-Jun-15.
  */
 public class Downloader {
 
     private final Context context;
-    private final ArrayList<String> downloadQueue;
+    private final ArrayList<DownloadItems> downloadQueue;
     private static Downloader imageDownloader;
     private final String storagePath;
 
+    /*@param: context of the caller activity
+     */
     private Downloader(Context context) {
         downloadQueue = new ArrayList<>();
         this.context = context;
+
+        //create the storage dir where we are going to store the downloaded data
         storagePath = context.getExternalCacheDir().getAbsolutePath() + "/downloader";
         File dir = new File(storagePath);
         dir.mkdir();
     }
 
-    /* Static 'instance' method */
+    /* Static 'instance' method
+    * this is a singleton function
+    */
     public static Downloader getInstance(Context context) {
+        //get the singleton instance of the activity
         if (imageDownloader == null)
             imageDownloader = new Downloader(context);
         return imageDownloader;
     }
 
-    public void addNewUrl(String urlToDownload) {
-        downloadQueue.add(urlToDownload);
+    public void addNewUrl(final String urlOfImage, final ImageView imageView) {
+        //Add new url and respective imageView to downloadQueue
+        DownloadItems downloadItems = new DownloadItems();
+        downloadItems.imageView = imageView;
+        downloadItems.url = urlOfImage;
+        downloadQueue.add(downloadItems);
         if (downloadFileAync.getStatus() != AsyncTask.Status.RUNNING) {
-            //if the size is 1, than start the download queue
+            //if no async task running than...
             downloadFileAync.execute();
         }
     }
 
-    AsyncTask<Void, String, Void> downloadFileAync = new AsyncTask<Void, String, Void>() {
-        public File file;
-
+    private AsyncTask<Void, DownloadItems, Void> downloadFileAync = new AsyncTask<Void, DownloadItems, Void>() {
         @Override
         protected Void doInBackground(Void... voids) {
-             /*--- this method downloads an Image from the given URL,
-              *  then decodes and returns a Bitmap object*/
+             /* this method downloads an Image from the given URL,
+              * than downloads the image and publish the update so that
+              * it can show the image in the given image viewer
+              */
 
             for (int i = 0; i < downloadQueue.size(); i++) {
                 Log.d("i", i + "");
-                URL url = null;
-                String urlDownload = downloadQueue.get(i);
-                try {
-                    url = new URL(urlDownload);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-
                 HttpURLConnection connection = null;
                 InputStream input = null;
                 OutputStream output = null;
+
+                //url of the image
+                final String urlDownload = downloadQueue.get(i).url;
+                File file = null;   //file that will store image downloaded
+
                 try {
-                    if (url != null)
-                        connection = (HttpURLConnection) url.openConnection();
+                    if (needDownload(urlDownload)) {
+                        Log.d("file", "downloading");
+                        //create URL
+                        URL url = null;
+                        try {
+                            url = new URL(urlDownload);
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
 
-                    if (connection != null) {
-                        //Connection
-                        connection.setDoInput(true);
-                        connection.setReadTimeout(10000);
-                        connection.connect();
+                        if (url != null) connection = (HttpURLConnection) url.openConnection();
 
-                        // download the file
-                        input = new BufferedInputStream(url.openStream());
+                        if (connection != null) {
+                            //Connection
+                            connection.setDoInput(true);
+                            connection.setReadTimeout(15000);
+                            connection.setConnectTimeout(15000);
+                            connection.connect();
 
-                        //Generating the file name
-                        String[] s = urlDownload.split("/");
-                        String filename = s[s.length - 1];
+                            // download the file
+                            input = new BufferedInputStream(url.openStream());
 
-                        //creating file to store image
-                        file = new File(storagePath, filename);
-                        output = new FileOutputStream(file);
+                            //write to the file
+                            file = generateFile(urlDownload);
+                            output = new FileOutputStream(file);
+                            int dataInt;
+                            while ((dataInt = input.read()) != -1) {
+                                output.write(dataInt);
+                            }
+                        } else {
+                            Log.d("file", "skipping");
+                            file = generateFile(urlDownload);
+                        }
 
-                        //write to the file
-                        int dataInt;
-                        byte[] byteArray;
-                        while ((dataInt = input.read()) != -1) {
-                            output.write(dataInt);
+                        //set last modified timestamp so will be useful to clear the cache
+                        file.setLastModified(System.currentTimeMillis());
+
+                        //publishing the update
+                        DownloadItems items = new DownloadItems();
+                        items.imageView = downloadQueue.get(i).imageView;
+                        items.imageBitmap = decodeBitmap(file);
+                        publishProgress(items);
+
+                        //clear the download queue if all the urls are downloaded
+                        if (i == downloadQueue.size() - 1) {
+                            Log.d("clear", "task");
+                            clearDownloadQueue();
+                            downloadFileAync.cancel(true);
                         }
                     }
                 } catch (IOException | NullPointerException e) {
+                    //delete the file if any exception generated
                     e.printStackTrace();
                 } finally {
                     if (connection != null) connection.disconnect();
@@ -113,10 +147,66 @@ public class Downloader {
             }
             return null;
         }
+
+        @Override
+        protected void onProgressUpdate(DownloadItems... downloadItemes) {
+            super.onProgressUpdate(downloadItemes);
+            Log.d("update", "published");
+            //show the bitmap into imageViewer
+            downloadItemes[0].imageView.setImageBitmap(downloadItemes[0].imageBitmap);
+        }
     };
 
-    class downloadItems {
+    public void clearDownloadQueue() {
+        //clear the download queue
+        downloadQueue.clear();
+    }
+
+    public boolean deleteCacheFile(final String url) {
+        return generateFile(url).delete();
+    }
+
+    private boolean needDownload(final String url) {
+        //Generating the file name
+        String[] s = url.split("/");
+        String filename = s[s.length - 1];
+        return !new File(storagePath + "/" + filename).exists();
+    }
+
+    private File generateFile(String url) {
+        //Generating the file name
+        String[] s = url.split("/");
+        String filename = s[s.length - 1];
+
+        //creating file to store image
+        return new File(storagePath, filename);
+    }
+
+    private Bitmap decodeBitmap(final File file) {
+        //Decoding the file into bitmap
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inDither = false;                     //Disable Dithering mode
+        //noinspection deprecation
+        options.inPurgeable = true;                   //Tell to gc that whether it needs free memory, the Bitmap can be cleared
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        return BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+    }
+
+    public void deleteUnusedFile() {
+        //delete all unused files
+        String[] fileList = new File(storagePath).list();
+        for (String s : fileList) {
+            File f = new File(storagePath + "/" + s);
+            if ((System.currentTimeMillis() - f.lastModified()) > 1000) {
+                f.delete();
+            }
+        }
+
+    }
+
+    class DownloadItems {
         String url;
         ImageView imageView;
+        Bitmap imageBitmap;
     }
 }
--- app/src/main/java/kevalpatel2106/urlimagedownloader/MainActivity.java
+++ app/src/main/java/kevalpatel2106/urlimagedownloader/MainActivity.java
@@ -1,14 +1,20 @@
 package kevalpatel2106.urlimagedownloader;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
     private static Downloader imageDownloader;
+    private ImageView imageView1;
+    private ImageView imageView2;
+    private ImageView imageView3;
+    private ImageView imageView4;
+    private ImageView imageView5;
+    private ImageView imageView6;
+    private ImageView imageView7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageDownloader = Downloader.getInstance(this);
 
-        String[] textUrls = {"http://www.gettyimages.in/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg",
-                "https://www.ucl.ac.uk/news/news-articles/1213/muscle-fibres-heart.jpg",
-                "http://clarklabs.org/images/carousel-image-6.jpg",
-                "http://www.hdwallpapersimages.com/wp-content/uploads/2014/01/Winter-Tiger-Wild-Cat-Images.jpg",
-                "http://3.bp.blogspot.com/-rZmCIp0C-hQ/Tx6aCFeweoI/AAAAAAAAAnQ/WqIEVBTIzRk/s1600/Cool-Tiger-Wallpaper-1920x1080-HD.jpg",
-                "http://www.zastavki.com/pictures/originals/2013/Photoshop_Image_of_the_horse_053857_.jpg",
+        imageView1 = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
+        imageView4 = (ImageView) findViewById(R.id.imageView4);
+        imageView5 = (ImageView) findViewById(R.id.imageView5);
+        imageView6 = (ImageView) findViewById(R.id.imageView6);
+        imageView7 = (ImageView) findViewById(R.id.imageView7);
+    }
+
+    public void loadnew(View view) {
+
+        String[] textUrls = {"http://buzztcdn.blob.core.windows.net/gpics/group-default-thumb.png",
+                "http://buzztcdn.blob.core.windows.net/gpics/fd23032c-23de-4c1b-bbec-d4ff6753c094-thumb.jpg",
+                "http://38.media.tumblr.com/avatar_c5bd40de9664_64.png",
+                "http://buzztcdn.blob.core.windows.net/gpics/group-default-thumb.png",
+                "http://buzztcdn.blob.core.windows.net/gpics/fd23032c-23de-4c1b-bbec-d4ff6753c094-thumb.jpg",
+                "http://38.media.tumblr.com/avatar_c5bd40de9664_64.png",
                 "http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg"};
-        for (String textUrl : textUrls) {
-            imageDownloader.addNewUrl(textUrl);
+
+        ImageView[] imageViews = {imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7};
+        for (int i = 0; i < textUrls.length; i++) {
+            imageDownloader.addNewUrl(textUrls[i], imageViews[i]);
         }
     }
 }

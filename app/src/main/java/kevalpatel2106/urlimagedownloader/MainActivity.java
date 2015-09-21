8b869288e4f333b7a492dccf583516e65e450093
Keval Patel <keval@buzztown.me>
2015-09-21 15:13:55 +0530

    example url updated

--- app/src/main/java/kevalpatel2106/urlimagedownloader/MainActivity.java
+++ app/src/main/java/kevalpatel2106/urlimagedownloader/MainActivity.java
@@ -1,11 +1,9 @@
 package kevalpatel2106.urlimagedownloader;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -16,42 +14,43 @@ import android.widget.Toast;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private String[] mImageUrls;
+    private ImageView[] mImageViews;
+    private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Context context = this;
-
-        Downloader imageDownloader = Downloader.getInstance(this);
+        mContext = this;
 
         //initialize view
         ImageView imageView1 = (ImageView) findViewById(R.id.imageView);
         ImageView imageView2 = (ImageView) findViewById(R.id.imageView2);
         ImageView imageView3 = (ImageView) findViewById(R.id.imageView3);
         ImageView imageView4 = (ImageView) findViewById(R.id.imageView4);
-        ImageView imageView5 = (ImageView) findViewById(R.id.imageView5);
-        ImageView imageView6 = (ImageView) findViewById(R.id.imageView6);
-        ImageView imageView7 = (ImageView) findViewById(R.id.imageView7);
 
-        //urls to download
-        String[] textUrls = {"http://buzztcdn.blob.core.windows.net/gpics/group-default-thumb.png",
-                "http://buzztcdn.blob.core.windows.net/gpics/fd23032c-23de-4c1b-bbec-d4ff6753c094-thumb.jpg",
-                "http://38.media.tumblr.com/avatar_c5bd40de9664_64.png",
-                "http://buzztcdn.blob.core.windows.net/gpics/group-default-thumb.png",
-                "http://buzztcdn.blob.core.windows.net/gpics/fd23032c-23de-4c1b-bbec-d4ff6753c094-thumb.jpg",
-                "http://38.media.tumblr.com/avatar_c5bd40de9664_64.png",
-                "http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg"};
+        //urls to download (This all urls are for example.)
+        mImageUrls = new String[]{"http://www.gettyimages.in/gi-resources/images/Homepage/Category-Creative/UK/UK_Creative_462809583.jpg",
+                "http://www.hdwallpapersimages.com/wp-content/uploads/2014/01/Winter-Tiger-Wild-Cat-Images.jpg",
+                "http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg",
+                "http://saturn.jpl.nasa.gov/multimedia/images/moons/images/PIA07759.jpg"};
 
         //image views to display downloaded images. (you can pass null if you don't want to display images.)
-        ImageView[] imageViews = {imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7};
+        mImageViews = new ImageView[]{imageView1, imageView2, imageView3, imageView4};
+    }
+
+    public void startLoadImage(View view) {
+        Downloader imageDownloader = Downloader.getInstance(this);
 
         //add to the download queue
-        for (int i = 0; i < textUrls.length; i++) {
+        for (int i = 0; i < mImageUrls.length; i++) {
             final int finalI = i;
-            imageDownloader.addNewUrl(textUrls[i], imageViews[i], true, new UrlBitmapListener() {
+            imageDownloader.addNewUrl(mImageUrls[i], mImageViews[i], true, new UrlBitmapListener() {
                 @Override
                 public void onBitmapDownloaded(String url, Bitmap bitmap) {
-                    Toast.makeText(context, "Image " + finalI + " downloaded", Toast.LENGTH_SHORT).show();
+                    //a listener to call when image bitmap is downloaded
+                    Toast.makeText(mContext, "Image " + finalI + " downloaded", Toast.LENGTH_SHORT).show();
                 }
             });
         }
